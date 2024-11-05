1350faae633f738bff16922a60fe10552e197ab9
Jekabs Ilkens <jekabs4442@gmail.com>
2024-11-05 21:54:47 +0200

    Migration update and better data for factories and seeder

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -17,7 +17,17 @@ class CommentFactory extends Factory
                 "I completely disagree... a bunch of nonsense.",
                 "Thanks for sharing! It was very helpful :)",
                 "If you are interested in a business deal you can contact me through greg.tech@mail.com",
-                "What language is this? Can someone translate it for me?"
+                "What language is this? Can someone translate it for me?",
+                "I want to learn this kind of style! Teach me.",
+                "Awesome article! Im looking forward to reading more from you.",
+                "This is a fantastic post! I learned so much about the topic.",
+                "There are multiple typos in the 2nd paragraph... just letting you know.",
+                "I recently contacted the post author and he gave me some spoilers for the next post.. he he he :)",
+                "Did anyone start noticing these facts in their everyday life since reading this ????",
+                "Thanks for the tips! I will definitely try them out.",
+                "I think you missed a few key points here. It would be good to expand on this topic.",
+                "Interesting perspective, but it doesn't fully address the complexities of the issue.",
+                "This post did not really resonate with me. I expected something more engaging."
             ]),
             'user_id' => User::factory(),
             'post_id' => Post::factory()
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,46 +12,56 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Category::create(['name' => 'Business', 'slug' => 'business']);
-        Category::create(['name' => 'Finance', 'slug' => 'finance']);
-        Category::create(['name' => 'Marketing', 'slug' => 'marketing']);
-        Category::create(['name' => 'Education', 'slug' => 'education']);
-        Category::create(['name' => 'Science', 'slug' => 'science']);
-        Category::create(['name' => 'Environment', 'slug' => 'environment']);
-        Category::create(['name' => 'Travel', 'slug' => 'travel']);
-        Category::create(['name' => 'Lifestyle', 'slug' => 'lifestyle']);
-        Category::create(['name' => 'Fashion', 'slug' => 'fashion']);
-        Category::create(['name' => 'Beauty', 'slug' => 'beauty']);
-        Category::create(['name' => 'Food', 'slug' => 'food']);
-        Category::create(['name' => 'Recipes', 'slug' => 'recipes']);
-        Category::create(['name' => 'Fitness', 'slug' => 'fitness']);
-        Category::create(['name' => 'Relationships', 'slug' => 'relationships']);
-        Category::create(['name' => 'Personal Development', 'slug' => 'personal-development']);
-        Category::create(['name' => 'Mental Health', 'slug' => 'mental-health']);
-        Category::create(['name' => 'How To', 'slug' => 'how-to']);
-        Category::create(['name' => 'Gardening', 'slug' => 'gardening']);
-        Category::create(['name' => 'Pets', 'slug' => 'pets']);
-        Category::create(['name' => 'Real Estate', 'slug' => 'real-estate']);
-        Category::create(['name' => 'Entertainment', 'slug' => 'entertainment']);
-        Category::create(['name' => 'Books', 'slug' => 'books']);
-        Category::create(['name' => 'Movies', 'slug' => 'movies']);
-        Category::create(['name' => 'Music', 'slug' => 'music']);
-        Category::create(['name' => 'Art', 'slug' => 'art']);
-        Category::create(['name' => 'Photography', 'slug' => 'photography']);
+        $categories = [
+            ['name' => 'Business'],
+            ['name' => 'Finance'],
+            ['name' => 'Marketing'],
+            ['name' => 'Education'],
+            ['name' => 'Science'],
+            ['name' => 'Environment'],
+            ['name' => 'Travel'],
+            ['name' => 'Lifestyle'],
+            ['name' => 'Fashion'],
+            ['name' => 'Beauty'],
+            ['name' => 'Food'],
+            ['name' => 'Recipes'],
+            ['name' => 'Fitness'],
+            ['name' => 'Relationships'],
+            ['name' => 'Personal Development'],
+            ['name' => 'Mental Health'],
+            ['name' => 'How To'],
+            ['name' => 'Gardening'],
+            ['name' => 'Pets'],
+            ['name' => 'Real Estate'],
+            ['name' => 'Entertainment'],
+            ['name' => 'Books'],
+            ['name' => 'Movies'],
+            ['name' => 'Music'],
+            ['name' => 'Art'],
+            ['name' => 'Photography'],
+        ];
 
-        $users = User::factory()->count(5)->create();
-
-        $users->each(function ($user) use ($users) 
+        foreach ($categories as $category) 
         {
-            $posts = Post::factory()->count(rand(1, 3))->create(['author_id' => $user->id]);
+            Category::create($category);
+        }
 
-            $posts->each(function ($post) use ($users) 
+        $allCategories = Category::all();
+        $users = User::factory()->count(7)->create();
+        
+        $users->each(function ($user) use ($allCategories, $users)
+        {
+            $posts = Post::factory()->count(rand(1, 2))->create(['author_id' => $user->id]);
+        
+            $posts->each(function ($post) use ($allCategories, $users)
             {
-                $commentingUsers = $users->random(rand(1, 4));
-                
+                $randomCategories = $allCategories->random(rand(1, 5))->pluck('id');
+                $post->categories()->attach($randomCategories);
+        
+                $commentingUsers = $users->random(rand(1, 5));
                 foreach ($commentingUsers as $commentingUser) 
                 {
-                    Comment::factory()->count(rand(1, 2))->create([
+                    Comment::factory()->count(rand(1, 1))->create([
                         'post_id' => $post->id,
                         'user_id' => $commentingUser->id,
                     ]);

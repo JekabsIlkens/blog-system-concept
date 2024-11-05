6356f3d398dac0c7e98b29582468f9539976fc33
Jekabs Ilkens <jekabs4442@gmail.com>
2024-11-05 12:27:30 +0200

    New comment input form and section for all comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -40,6 +40,40 @@
                         </form>
                     @endif
                 </div>
+
+                <div class="group relative flex items-center mx-4 my-4 border-t border-neutral-200">
+                    <form action="#" method="POST" class="w-full mt-4 px-4 py-4 border-solid border-0 border-neutral-200 rounded-md bg-neutral-100">
+                        @csrf                          
+                        <div>
+                            <div class="flex items-center justify-between">
+                                <label for="comment" class="block text-sm/6 font-medium text-neutral-900">Leave a comment:</label>
+                            </div>
+                            <div class="mt-2">
+                                <textarea id="comment" name="comment" type="text" value="{{ old('comment') }}" class="block w-full rounded-md border-0 px-1.5 py-1.5 text-neutral-900 shadow-sm ring-1 ring-inset ring-neutral-300 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm/6"></textarea>
+                            </div>
+                            @error('comment')
+                                <div class="block text-sm/6 font-medium text-amber-600">{{ $message }}</div>
+                            @enderror
+                            @if ($errors->has('error'))
+                                <div class="block text-sm/6 font-medium text-amber-600">{{ $errors->first('error') }}</div>
+                            @endif
+                        </div>
+                          
+                        <div class="group relative flex items-center mt-4">
+                            <button type="submit" class="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600">
+                                Add
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="group relative flex items-center mx-4 my-4 border-t border-neutral-200">
+                    <article class="w-full mt-4 border-solid border-0 border-neutral-200 rounded-md bg-neutral-100">
+                        <p class="text-base text-neutral-600 mx-4 my-4">
+                            Comments go here!
+                        </p>
+                    </article>
+                </div>
             </div>
         </div>     
     </main>

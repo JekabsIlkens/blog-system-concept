b7c97e067abf8b4aa0bda0dfb782ae768b5874b7
Jekabs Ilkens <jekabs4442@gmail.com>
2024-11-15 21:44:06 +0200

    Welcome page visual update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <div class="relative isolate px-6 pt-14 lg:px-8 bg-white shadow-md rounded-md">
+        <div class="relative isolate px-6 pt-14 lg:px-8 bg-[url('https://i.postimg.cc/ZqsfdpgT/bsc-welcome.jpg')] bg-cover bg-center shadow-md rounded-md">
             <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                 <div class="text-center">
 
-                    <h1 class="text-balance text-5xl font-semibold tracking-tight text-neutral-800 sm:text-7xl">
+                    <h1 class="text-balance text-5xl font-semibold tracking-tight text-neutral-800 sm:text-6xl">
                         Blog System Concept
                     </h1>
 
-                    <p class="mt-8 text-pretty text-lg font-medium text-neutral-600 sm:text-xl/8">
+                    <p class="mt-8 pl-8 pr-8 text-pretty text-lg font-medium text-neutral-600 sm:text-lg/6">
                         Share your ideas and knowledge with the world by creating your own blog posts 
                         and indulge in interesting conversations with other like-minded people!
                     </p>
